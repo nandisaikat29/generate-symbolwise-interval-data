00b4182ca7d05b6e4fa91d4a7ec856efd7e0467e
Saikat Nandi <60563663+nandisaikat29@users.noreply.github.com>
2021-04-02 13:37:56 +0530

    Update price_monitor.cpp
    done all requirements

--- price_monitor.cpp
+++ price_monitor.cpp
@@ -7,7 +7,7 @@
 #include<ostream>
 #include<stdlib.h>
 #include<string.h>
-#include<stdint.h>	
+#include<stdint.h>
 using namespace std;
 void tokenizer(string const &str, const char delim,vector<string> &tokens)
 {
@@ -24,32 +24,24 @@ void tokenizer(string const &str, const char delim,vector<string> &tokens)
     }
 }
 double time_sec(string time_part){
-		vector<string> time;
+                vector<string> time;
+                double test;
                 stringstream check1(time_part);
                 string time_slice;
 
-	         while (getline(check1, time_slice, ':') )
+                 while (getline(check1, time_slice, ':') )
                         {
-                      
+
                                 time.push_back(time_slice);
 
-                  	}
-                stringstream geek(time[0]);
-               	double h=0;
-                geek>>h;
-		
-                stringstream geek1(time[1]);
-                double m=0;
-                geek1>>m;
-
-                stringstream geek2(time[2]);
-                double s=0;
-                geek2>>s;
-	
-		double t_present=h*3600+m*60+s;
-		
-		return t_present;
-}
+                        }
+                double h=atof(time[0].c_str());
+                double m=atof(time[1].c_str());
+                double s=atof(time[2].c_str());
+                double t_present=h*3600+m*60+s;
+                sscanf(to_string(t_present).c_str(),"%12lf",&test);
+                return test;
+	}
 int main(){
         string FirstFile,output;
         int j=0;
@@ -59,69 +51,71 @@ int main(){
         cout<<"Enter output file name\n";
         cin>>output;
         ifstream MyReadFile(FirstFile.c_str());
-	ifstream MyReadFile1(FirstFile.c_str());
+        ifstream MyReadFile1(FirstFile.c_str());
         ofstream ob(output.c_str());
-	double t_prev;
-	list<string> m;
-	int n;
-       	double t_present;
-	m.push_back("0");
-	string myText,myText1;
-	const char delim=',';
-	while(1){
-		int i=0;
-		vector<string> token;
-                getline(MyReadFile, myText);	
-		j++;
-		tokenizer(myText,delim,token);
-     		if(MyReadFile.eof()){	
+        double t_prev;
+        list<string> m;
+        int n;
+        m.push_back("0");
+        double t_present;
+        const char delim=',';
+        string myText,myText1;
+        while(1){
+                int i=0;
+                vector<string> token;
+                getline(MyReadFile, myText);
+                j++;
+                tokenizer(myText,delim,token);
+                if(MyReadFile.eof()){
                         break;
-		}
-		if(token.size()<2)
-			continue;	
-		const bool is_in = find(m.begin(), m.end(),token[1]) != m.end();
-		if(is_in==1){
-			continue;
-		}
-		else{
-			m.push_back(token[1]);
-			ob<<myText;
-			ob<<"\n";
-			t_prev=time_sec(token[0]);
-			t_present=time_sec(token[0]);
-			int v=0;
-			while(v<j-1){
-        		      	getline(MyReadFile1, myText1);
-				v++;
-			}
-			while(1){
-				int l=0;
-				vector<string> token1;
-				getline(MyReadFile1, myText1);
-				tokenizer(myText1,delim,token1);
-				 if(MyReadFile1.eof()){	
-                     			   break;
-				}
-					if(token1[1].compare(m.back())==0){
-						t_present=time_sec(token1[0]);
-						if(t_present-t_prev>=1){
-							ob<<myText1;
-							ob<<"\n";
-							
-						}
-					}
-					else{
-						continue;	
-					}	
-					t_prev=t_present;
-				token1.clear();
-	
-			}
-			MyReadFile1.clear();
-      			MyReadFile1.seekg(0);	
-		}
-		token.clear();
-	}
- 	ob.close();
- 	return 0;
-}		
+                }
+                if(token.size()<2)
+                        continue;
+                const bool is_in = find(m.begin(), m.end(),token[1]) != m.end();
+                if(is_in==1){
+                        continue;
+                }
+                else{
+                        m.push_back(token[1]);
+                        ob<<myText;
+                        ob<<"\n";
+                        t_prev=time_sec(token[0]);
+                        t_present=time_sec(token[0]);
+                        int v=0;
+                        while(v<j-1){
+        			getline(MyReadFile1, myText1);
+                                v++;
+                        }
+                        while(1){
+                                int l=0;
+                                vector<string> token1;
+                                getline(MyReadFile1, myText1);
+                                tokenizer(myText1,delim,token1);
+                                 if(MyReadFile1.eof()){
+                                           break;
+                                }
+                                        if(token1[1].compare(m.back())==0){
+                                                t_present=time_sec(token1[0]);
+                                                if(t_present-t_prev>=1){
+                                                        ob<<myText1;
+                                                        ob<<"\n";
+
+                                                }
+                                        }
+                                        else{
+                                                continue;
+                                        }
+                                        t_prev=t_present;
+                                        token1.clear();
+
+                        }
+                        MyReadFile1.clear();
+                        MyReadFile1.seekg(0);
+                }
+                token.clear();
+        }
+        ob.close();
+        return 0;
+}
+                                         
+
